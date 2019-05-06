7fcf481b2d27da714f618f1ecc520325057c6fea
abolfazl <asad.abolfazl.asad@gmail.com>
2019-05-06 20:27:22 +0430

    Merge branch 'master' of F:\project-A.R.A with conflicts.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import Exceptions.CustomException;
-import Exceptions.WrongCommandException;
 import Menus.Cammands.Command;
 import Menus.MenuManager;
 import Menus.Menus;
@@ -10,12 +9,10 @@ import static Menus.GameMode.SINGLE_PLAYER;
 public class Main {
     public static void main(String[] args) {
         while (true) {
-            boolean matchACommand = false;
-            System.out.println(MenuManager.getCurrentMenu().getTitle()); // todo as debug
             try {
                 String commandLine;
                 if (MenuManager.getCurrentMenuType().equals(Menus.BATTLE)) {
-//                if (MenuManager.getGameMode().equals(SINGLE_PLAYER) && MenuManager.getCurrentMatch().getOwnPlayer() == null)
+//                if (MenuManager.getGameMode().equals(SINGLE_PLAYER))
 //                    commandLine = AI.getCommand();
 //                else
                     commandLine = View.getInstance().getCommand();
@@ -23,12 +20,8 @@ public class Main {
                     commandLine = View.getInstance().getCommand();
                 for (Command command : MenuManager.getCurrentMenu().getMenuCommands()) {
                     command.setMatcher(command.getPattern().matcher(commandLine));
-                    if (command.getMatcher().matches()) {
+                    if (command.getMatcher().matches())
                         command.execute();
-                        matchACommand = true;
-                    }
-                    if(!matchACommand)
-                        throw new WrongCommandException();
                 }
             } catch (CustomException e) {
                 e.printStackTrace();

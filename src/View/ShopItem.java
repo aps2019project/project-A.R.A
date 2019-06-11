8be3f69c120e717de27121bb21b15523867b7c2a
re0078 <reza.abdollahzadeh@gmail.com>
2019-06-11 20:24:04 +0430

    can select cards

--- src/View/ShopItem.java
+++ src/View/ShopItem.java
@@ -3,10 +3,7 @@ package View;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.effect.BoxBlur;
-import javafx.scene.effect.Glow;
-import javafx.scene.effect.InnerShadow;
-import javafx.scene.effect.Reflection;
+import javafx.scene.effect.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -38,6 +35,28 @@ public class ShopItem extends StackPane {
         addProperty("Attack Type", "ranged");
         addProperty("Attack Type", "ranged");
         addProperty("Attack Type", "ranged");
+
+        this.setOnMousePressed(e->{
+            setSelected(!selected);
+
+            if(this.getEffect() == null) {
+                Bloom bloom = new Bloom(0);
+                bloom.setInput(new ColorAdjust(0.61, 0.5, 0, 0.5));
+                this.setEffect(bloom);
+            }else this.setEffect(getDefaultCardEffect());
+        });
+    }
+
+    private Effect getDefaultCardEffect(){
+        Bloom bloom = new Bloom(0);
+        if(this.isOwned()){
+            bloom.setInput(new ColorAdjust(-0.46, 0.5, 0, 0.5));
+        }
+        else{
+//            bloom.setInput(new ColorAdjust(-0.21, 0.5, 0.5, 0.5));
+            bloom = null;
+        }
+        return null;
     }
 
     private void addAttributeBox() {
